a9099f5eb71953b5036d5923ebec004e74a6c34e
Miftakur Rohman <kurrosetra@gmail.com>
2018-03-22 19:12:01 +0700

    update
    - add example pid files
    - downgrade u8g2 library to u8g
    - add main page
    - add menu page (incomplete)

--- Firmwares/mainTrain/menuDisplay.cpp
+++ Firmwares/mainTrain/menuDisplay.cpp
@@ -11,8 +11,7 @@ void menuDisplay::init()
 {
 	pinMode(_lcd_backligth_pin, OUTPUT);
 	backlight(1);
-	lcd->begin();
-	lcd->setFont(u8g2_font_ncenB10_tr);
+	trainDetail.menuLang=ENGLISH_LANG;
 }
 
 void menuDisplay::backlight(bool onf)
@@ -20,108 +19,233 @@ void menuDisplay::backlight(bool onf)
 	digitalWrite(_lcd_backligth_pin, onf);
 }
 
-void menuDisplay::pageMain(byte master, byte gps)
+void menuDisplay::pageUpdate()
+{
+	lcd->firstPage();
+	do {
+		pageMenu(0);
+	} while (lcd->nextPage());
+//	if (displayState != currentDisplayState) {
+//		lcd->firstPage();
+//		do {
+//			switch (displayState)
+//			{
+//			case DISP_IDLE:
+//				if (currentDisplayState != DISP_MAIN)
+//					pageMain();
+//				backlight(LOW);
+//				break;
+//			case DISP_MAIN:
+//				backlight(HIGH);
+//				pageMain();
+//				currentMenuPointer = 0;
+//				menuPointer = 0;
+//				break;
+//			case DISP_MENU:
+//				if (menuPointer != currentMenuPointer) {
+//					pageMenu(menuPointer);
+//					currentMenuPointer = menuPointer;
+//				}	//(menuPointer != currentMenuPointer)
+//				break;
+//			case DISP_MENU_MENU_LANGUAGE:
+//				break;
+//			case DISP_MENU_DISPLAY_LANGUAGE:
+//				break;
+//			case DISP_MENU_TRAIN_NAME:
+//				break;
+//			case DISP_MENU_TRAIN_ROUTE:
+//				break;
+//			case DISP_MENU_CURRENT_STATION:
+//				break;
+//			case DISP_MENU_MASTER_MODE:
+//				break;
+//			case DISP_MENU_GPS_MODE:
+//				break;
+//			case DISP_MENU_COACH:
+//				break;
+//			case DISP_MENU_COACH_NUMBER:
+//				break;
+//			case DISP_MENU_CHANGE_REQ:
+//				break;
+//			case DISP_MENU_SYSTEM_INFO:
+//				break;
+//			default:
+//				break;
+//			}
+//		} while (lcd->nextPage());
+//
+//		currentDisplayState = displayState;
+//	}	//(displayState != currentDisplayState)
+
+}
+
+void menuDisplay::setMenuPointer(byte pos)
+{
+	menuPointer = pos;
+}
+
+byte menuDisplay::getMenuPointer()
+{
+	return currentMenuPointer;
+}
+
+void menuDisplay::pageMain()
 {
 	String s;
-
-	lcd->clearBuffer();
+	u8g_uint_t w, d;
 
 	//HEADER
-	lcd->setFont(u8g2_font_ncenB08_tf);
-	s = F("Bangladesh Railway");
-	lcd->drawStr(0, 10, s.c_str());
+	w = lcd->getWidth();
+	lcd->setFont(u8g_font_ncenB08);
+	lcd->setFontRefHeightText();
+	lcd->setFontPosTop();
+	if (trainDetail.menuLang == ENGLISH_LANG)
+		s = F("Bangladesh Railway");
+	else
+		s = F("~Bangladesh Railway~");
+	d = (w - lcd->getStrWidth(s.c_str())) / 2;
+	lcd->drawStr(d, 0, s.c_str());
+
 	//SUB-HEADER
-	lcd->setFont(u8g2_font_5x7_tr);
-	s = F("Display Controller");
-	lcd->drawStr(0, 25, s.c_str());
+	lcd->setFont(u8g_font_5x7r);
+	if (trainDetail.menuLang == ENGLISH_LANG)
+		s = F("Display Controller");
+	else
+		s = F("~Display Controller~");
+	d = (w - lcd->getStrWidth(s.c_str())) / 2;
+	lcd->drawStr(d, 25, s.c_str());
 	//KONTEN 1
 	//KONTEN 2
-	if (master)
-		s = F("Master");
+	if (trainDetail.masterMode) {
+		if (trainDetail.menuLang == ENGLISH_LANG)
+			s = F("Master");
+		else
+			s = F("~Master~");
+	}
+	else {
+		if (trainDetail.menuLang == ENGLISH_LANG)
+			s = F("Slave");
+		else
+			s = F("~Slave~");
+	}
+	if (trainDetail.menuLang == ENGLISH_LANG)
+		s += ';';
 	else
-		s = F("Slave");
-	s += ';';
-	if (gps)
-		s += F("GPS");
-	else
-		s += F("Manual");
-	lcd->setFont(u8g2_font_5x7_tr);
-	lcd->drawStr(0, 64, s.c_str());
-
-	lcd->sendBuffer();
+		s += "~;~";
+	if (trainDetail.gpsMode) {
+		if (trainDetail.menuLang == ENGLISH_LANG)
+			s += F("GPS");
+		else
+			s += F("~GPS~");
+	}
+	else {
+		if (trainDetail.menuLang == ENGLISH_LANG)
+			s += F("Manual");
+		else
+			s += F("~Manual~");
+	}
+	d = (w - lcd->getStrWidth(s.c_str())) / 2;
+	lcd->drawStr(d, 63, s.c_str());
 }
 
-void menuDisplay::pageSubMenu(String title, String opt1, String opt2,
-		byte selection)
+void menuDisplay::pageMenu(byte posMenu)
 {
-	int i;
-
-	lcd->clearBuffer();
+	String s[3];
 
 	//HEADER
-	lcd->setFont(u8g2_font_ncenB08_tf);
-	lcd->drawStr(0, 10, title.c_str());
-	lcd->setFont(u8g2_font_5x7_tf);
+	lcd->setFont(u8g_font_ncenB08);
+	lcd->setFontRefHeightText();
+	lcd->setFontPosTop();
+	if (trainDetail.menuLang == ENGLISH_LANG)
+		s[0] = F("Select Menu:");
+	else
+		s[0] = F("~Select Menu~");
+	lcd->drawStr(0, 0, s[0].c_str());
 
-	//String 1
-	if (selection) {
-		lcd->setFontMode(1);
-		lcd->setDrawColor(2);
-		i = lcd->getStrWidth(opt1.c_str());
-		lcd->drawBox(0, 37, i + 1, 10);
+	//MENU CONTENT
+	if (trainDetail.menuLang == ENGLISH_LANG)
+		s[1] = menu_setting_en[posMenu];
+	else
+		s[1] = menu_setting_bl[posMenu];
+
+	if (posMenu == 0) {
+		if (trainDetail.menuLang == ENGLISH_LANG) {
+			s[0] = menu_setting_en[menu_setting_length - 1];
+			s[2] = menu_setting_en[posMenu + 1];
+		}
+		else {
+			s[0] = menu_setting_bl[menu_setting_length - 1];
+			s[2] = menu_setting_bl[posMenu + 1];
+		}
 	}
-	lcd->drawStr(0, 45, opt1.c_str());
-
-	//String 2
-	if (!selection) {
-		lcd->setFontMode(1);
-		lcd->setDrawColor(2);
-		i = lcd->getStrWidth(opt2.c_str());
-		lcd->drawBox(0, 52, i + 1, 10);
+	else if (posMenu >= menu_setting_length - 1) {
+		if (trainDetail.menuLang == ENGLISH_LANG) {
+			s[0] = menu_setting_en[posMenu - 1];
+			s[2] = menu_setting_en[0];
+		}
+		else {
+			s[0] = menu_setting_bl[posMenu - 1];
+			s[2] = menu_setting_bl[0];
+		}
 	}
-	lcd->drawStr(0, 60, opt2.c_str());
 
-	lcd->sendBuffer();
+	lcd->setFont(u8g_font_5x7);
+	lcd->drawStr(0, 30, s[0].c_str());
+	lcd->setDefaultForegroundColor();
+	lcd->drawBox(0, 37, lcd->getStrWidth(s[1].c_str()) + 1, 10);
+	lcd->setDefaultBackgroundColor();
+	lcd->drawStr(1, 45, s[1].c_str());
+	lcd->setDefaultForegroundColor();
+	lcd->drawStr(2, 60, s[2].c_str());
+
+//	//String 1
+//	if (selection == 1) {
+//		lcd->setFontMode(1);
+//		lcd->setDrawColor(2);
+//		i = lcd->getStrWidth(opt1.c_str());
+//		lcd->drawBox(0, 22, i + 1, 10);
+//	}
+//	lcd->drawStr(0, 30, opt1.c_str());
 }
 
-void menuDisplay::pageSubMenu(String title, String opt1, String opt2,
-		String opt3, byte selection)
-{
-	int i = 0;
-
-	lcd->clearBuffer();
-
-	//HEADER
-	lcd->setFont(u8g2_font_ncenB08_tf);
-	lcd->drawStr(0, 10, title.c_str());
-	lcd->setFont(u8g2_font_5x7_tf);
-
-	//String 1
-	if (selection == 1) {
-		lcd->setFontMode(1);
-		lcd->setDrawColor(2);
-		i = lcd->getStrWidth(opt1.c_str());
-		lcd->drawBox(0, 22, i + 1, 10);
-	}
-	lcd->drawStr(0, 30, opt1.c_str());
-
-	//String 2
-	if (selection == 2) {
-		lcd->setFontMode(1);
-		lcd->setDrawColor(2);
-		i = lcd->getStrWidth(opt2.c_str());
-		lcd->drawBox(0, 37, i + 1, 10);
-	}
-	lcd->drawStr(0, 45, opt2.c_str());
-
-	//String 3
-	if (selection == 3) {
-		lcd->setFontMode(1);
-		lcd->setDrawColor(2);
-		i = lcd->getStrWidth(opt3.c_str());
-		lcd->drawBox(0, 52, i + 1, 10);
-	}
-	lcd->drawStr(0, 60, opt3.c_str());
-
-	lcd->sendBuffer();
-}
+//void menuDisplay::pageSubMenu(String title, String opt1, String opt2,
+//		String opt3, byte selection)
+//{
+//	int i = 0;
+//
+//	lcd->clearBuffer();
+//
+//	//HEADER
+//	lcd->setFont(u8g2_font_ncenB08_tf);
+//	lcd->drawStr(0, 10, title.c_str());
+//	lcd->setFont(u8g2_font_5x7_tf);
+//
+//	//String 1
+//	if (selection == 1) {
+//		lcd->setFontMode(1);
+//		lcd->setDrawColor(2);
+//		i = lcd->getStrWidth(opt1.c_str());
+//		lcd->drawBox(0, 22, i + 1, 10);
+//	}
+//	lcd->drawStr(0, 30, opt1.c_str());
+//
+//	//String 2
+//	if (selection == 2) {
+//		lcd->setFontMode(1);
+//		lcd->setDrawColor(2);
+//		i = lcd->getStrWidth(opt2.c_str());
+//		lcd->drawBox(0, 37, i + 1, 10);
+//	}
+//	lcd->drawStr(0, 45, opt2.c_str());
+//
+//	//String 3
+//	if (selection == 3) {
+//		lcd->setFontMode(1);
+//		lcd->setDrawColor(2);
+//		i = lcd->getStrWidth(opt3.c_str());
+//		lcd->drawBox(0, 52, i + 1, 10);
+//	}
+//	lcd->drawStr(0, 60, opt3.c_str());
+//
+//	lcd->sendBuffer();
+//}
--- Firmwares/mainTrain/menuDisplay.h
+++ Firmwares/mainTrain/menuDisplay.h
@@ -8,41 +8,108 @@
 #ifndef MENUDISPLAY_H_
 #define MENUDISPLAY_H_
 
-#include <U8g2lib.h>
 #include <SPI.h>
+#include "U8glib.h"
 
 class menuDisplay
 {
-private:
-	U8G2_ST7920_128X64_F_HW_SPI *lcd;
-
-	byte _lcd_backligth_pin;
-	static const byte menu_setting_length = 8;
-	String menu_setting_en[menu_setting_length] = { "Train Name", "Train Route",
-			"Current Station", "Display Language", "Menu Language",
-			"Master / Slave Mode", "Manual / GPS Mode", "System Info" };
-	String menu_setting_bl[menu_setting_length] = {
-
-	};
-//	String menu_setting_id[menu_setting_length] = { "Nama Kereta",
-//			"Jalur Kereta", "Stasiun Saat Init", "Bahasa Tampilan",
-//			"Bahasa Pilihan", "Mode Master/Slave", "Mode Manual/GPS",
-//			"Informasi Sistem" };
-
 public:
 	menuDisplay(byte ss, byte backlight)
 	{
-		lcd = new U8G2_ST7920_128X64_F_HW_SPI(U8G2_R0, ss);
+		lcd = new U8GLIB_ST7920_128X64_4X(ss);
 		_lcd_backligth_pin = backlight;
 	}
+
+	enum DISPLAY_STATE
+	{
+		DISP_IDLE,
+		DISP_MAIN,
+		DISP_MENU,
+		DISP_MENU_TRAIN_NAME,
+		DISP_MENU_TRAIN_ROUTE,
+		DISP_MENU_CURRENT_STATION,
+		DISP_MENU_DISPLAY_LANGUAGE,
+		DISP_MENU_MENU_LANGUAGE,
+		DISP_MENU_MASTER_MODE,
+		DISP_MENU_GPS_MODE,
+		DISP_MENU_SYSTEM_INFO,
+		DISP_MENU_COACH,
+		DISP_MENU_COACH_NUMBER,
+		DISP_MENU_CHANGE_REQ
+	};
+	byte displayState = DISP_MAIN;
+	enum MASTER_MODE
+	{
+		SLAVE_MODE,
+		MASTER_MODE
+	};
+
+	enum GPS_MODE
+	{
+		MANUAL_MODE,
+		GPS_MODE
+	};
+
+	enum LANGUAGE
+	{
+		ENGLISH_LANG,
+		BANGLADESH_LANG
+	};
+
+	static const byte MAX_COACH_LIST_ID = 20;
+	struct Coach_List
+	{
+		char *name;
+		byte howmany;
+	};
+
+	struct Train_Info
+	{
+		char * trainName;	// train's name
+		char * trainNum;	// train's number
+		char * currentStation;	// current station's name
+		uint16_t currentStationPosition;  // position of current station in "default.PID"
+		bool trainRoute;	// train's route (down=0, up=1)
+	};
+
+	struct Train_Detail
+	{
+		byte menuLang;		// menu language (0=en, 1=ba)
+		byte dispLang;		// display language (0=en, 1=ba)
+		Train_Info trainInfo;		// current train's info
+		Coach_List currentCoach;	// current coach's info
+		Coach_List coachList[MAX_COACH_LIST_ID];  // all available coach id and how many
+		bool masterMode;	// master/slave mode (slave=0, master=1)
+		bool gpsMode;		// gps/manual mode (manual=0, gps=1)
+	};
+	Train_Detail trainDetail;
+	byte menuPointer = 0;
 
 	void init();
 	void backlight(bool onf);
 
-	void pageMain(byte master, byte gps = 0);
-	void pageSubMenu(String title, String opt1, String opt2, byte selection);
-	void pageSubMenu(String title, String opt1, String opt2, String opt3,
-			byte selection);
+	void pageUpdate();
+	void setMenuPointer(byte pos);
+	byte getMenuPointer();
+
+private:
+	U8GLIB_ST7920_128X64_4X *lcd;
+
+	byte _lcd_backligth_pin;
+	static const byte menu_setting_length = 9;
+	const char *menu_setting_en[menu_setting_length] = { "Train Name",
+			"Train Route", "Current Station", "Display Language",
+			"Menu Language", "Master / Slave Mode", "Manual / GPS Mode",
+			"Coach List", "System Info" };
+	String menu_setting_bl[menu_setting_length] = {
+
+	};
+	byte currentDisplayState = DISP_IDLE;
+	Train_Detail tempTrainDetail;
+	byte currentMenuPointer = 0;
+
+	void pageMain();
+	void pageMenu(byte posMenu);
 
 };
 
