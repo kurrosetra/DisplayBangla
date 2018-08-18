f6ea6c54c4b4d66c0dc578ec0e5c80cae96b5c67
Miftakur Rohman <kurrosetra@gmail.com>
2018-08-19 02:25:06 +0700

    v1.2.0
    add:
    - gps function
    - libraries

--- Firmwares/mainTrain/train.h
+++ Firmwares/mainTrain/train.h
@@ -10,68 +10,71 @@
 
 #include "Arduino.h"
 
-#define COACH_NAME_MAX_SIZE						5
-#define COACH_NAME_ADDRESS						0
+#define COACH_NAME_MAX_SIZE            5
+#define COACH_NAME_ADDRESS            0
 
 typedef enum
 {
-	SLAVE_MODE,
-	MASTER_MODE
+  SLAVE_MODE,
+  MASTER_MODE
 } Master_Mode_e;
 
 typedef enum
 {
-	MANUAL_MODE,
-	GPS_MODE
+  MANUAL_MODE,
+  GPS_MODE
 } Gps_Mode_e;
 
 typedef enum
 {
-	STATION_ARRIVED = 0,
-	STATION_TOWARD = 1,
-	STATION_NEXT = 0,
-	STATION_PREV = 1
+  STATION_ARRIVED = 0,
+  STATION_TOWARD = 1,
+  STATION_NEXT = 0,
+  STATION_PREV = 1
 } Station_State_e;
 
 typedef struct
 {
-		uint16_t trainID;  								// train's number
-		String trainName;  								// train's name
-		String idBangla;
-		String nameBangla;
-		/* MASTER ONLY */
-		byte trainPosition;						// // position of current train's name in allTrainsName struct
+    uint16_t trainID;                 // train's number
+    String trainName;                 // train's name
+    String idBangla;
+    String nameBangla;
+    /* MASTER ONLY */
+    byte trainPosition;           // // position of current train's name in allTrainsName struct
 } Train_Info_t;
 
 typedef struct
 {
-		Station_State_e state;
-		String name;
-		String nameBangla;
-		uint16_t size;
-		String first;
-		String firstBangla;
-		String end;
-		String endBangla;
-		/* MASTER ONLY */
-		uint16_t pos;
+    Station_State_e state;
+    String name;
+    String nameBangla;
+    uint16_t size;
+    String first;
+    String firstBangla;
+    String end;
+    String endBangla;
+    /* MASTER ONLY */
+    uint16_t pos;
+    double lat;
+    double lon;
 } Station_Info_t;
 
 const byte FILES_MAX_SIZE = 20;
 typedef struct
 {
-		byte size;
-		uint16_t trainID[FILES_MAX_SIZE];
-		String trainName[FILES_MAX_SIZE];
+    byte size;
+    uint16_t trainID[FILES_MAX_SIZE];
+    String trainName[FILES_MAX_SIZE];
 } Train_Filename_t;
 
 typedef struct
 {
-		bool masterMode;
-		bool gpsMode;
-		String coachName;
-		Train_Info_t trainInfo;
-		Station_Info_t stationInfo;
+    bool masterMode;
+    bool gpsMode;
+    String coachName;
+    Train_Info_t trainInfo;
+    Station_Info_t stationInfo;
 } Train_Detail_t;
 
 #endif /* TRAIN_H_ */
+
